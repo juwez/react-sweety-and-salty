959f17f0044e35210315e986591d608ef0ab13ba
juwez <joeylybaert@outlook.com>
2022-02-09 17:29:36 +0100

    add custom in house colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
+content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+    theme: {
+    extend: {
+            colors: {
+              'in-house-red':'#F25252',
+              'in-house-orange':'#F38F1A',
+              'in-house-grey':'#3D3D3D',
+    },
   },
   plugins: [],
+}
 }
